44469ff2072874cec23edd349722fe9344e7e7ed
Vinicius Merelis <vinicius.merelis@basis.com.br>
2022-05-03 10:11:35 -0300

    Correção final no modulo de Turma

--- src/main/java/com/basis/sgc/service/mapper/CompetenciaEColaboradorMapper.java
+++ src/main/java/com/basis/sgc/service/mapper/CompetenciaEColaboradorMapper.java
@@ -1,22 +1,37 @@
 package com.basis.sgc.service.mapper;
 
-import com.basis.sgc.domain.CompetenciaColaborador;
+import com.basis.sgc.domain.TurmaCompetenciaColaborador;
 import com.basis.sgc.service.dto.CompetenciaEColaboradorDTO;
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface CompetenciaEColaboradorMapper extends EntityMapper<CompetenciaEColaboradorDTO, CompetenciaColaborador> {
+public interface CompetenciaEColaboradorMapper extends EntityMapper<CompetenciaEColaboradorDTO, TurmaCompetenciaColaborador> {
+
+    @Override
+    @Mapping(source = "id.competenciaId", target = "competencia.id")
+    @Mapping(source = "id.colaboradorId", target = "colaborador.id")
+    @Mapping(source = "competencia.nome", target = "competencia.nome")
+    @Mapping(source = "colaborador.nome", target = "colaborador.nome")
+    @Mapping(source = "colaborador.sobrenome", target = "colaborador.sobrenome")
+    CompetenciaEColaboradorDTO toDto(TurmaCompetenciaColaborador entity);
+
+    @Override
+    @InheritConfiguration
+    List<CompetenciaEColaboradorDTO> toDto(List<TurmaCompetenciaColaborador> entity);
 
     @Override
     @Mapping(source = "competencia.id", target = "id.competenciaId")
     @Mapping(source = "colaborador.id", target = "id.colaboradorId")
-    List<CompetenciaColaborador> toEntity(List<CompetenciaEColaboradorDTO> dto);
+    @Mapping(source = "competencia.nome", target = "competencia.nome")
+    @Mapping(source = "colaborador.nome", target = "colaborador.nome")
+    @Mapping(source = "colaborador.sobrenome", target = "colaborador.sobrenome")
+    TurmaCompetenciaColaborador toEntity(CompetenciaEColaboradorDTO dto);
 
     @Override
-    @Mapping(source = "id.competenciaId" ,target = "competencia.id")
-    @Mapping(source = "id.colaboradorId", target = "")
-    List<CompetenciaEColaboradorDTO> toDto(List<CompetenciaColaborador> entity);
+    @InheritConfiguration
+    List<TurmaCompetenciaColaborador> toEntity(List<CompetenciaEColaboradorDTO> dto);
 }
